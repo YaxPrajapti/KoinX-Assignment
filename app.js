fc5c723784fd733754044fe2329e770737b5b2f4
Yax Prajapati <92200237+YaxPrajapti@users.noreply.github.com>
2024-08-31 23:53:04 +0530

    updated package.json for deployment

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const transactionRouter = require("./router/transactionRouter");
-const expenseRouter = require('./router/expenseRouter'); 
+const expenseRouter = require("./router/expenseRouter");
 
-const {scheduler} = require("./cronScheduler");
+const { scheduler } = require("./cronScheduler");
 
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 
+// const corsOptions = {
+//   origin: allowedOrigins[env],
+//   method: "GET,POST,PUT,DELETE,PATCH,HEAD",
+//   credentials: true,
+// };
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/fetch", transactionRouter);
-app.use("/api/expense", expenseRouter)
+app.use("/api/expense", expenseRouter);
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
